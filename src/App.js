c7d92c603b872a00d82f82fbc03037aa0a8be387
rifanid98 <adninsijawa@gmail.com>
2020-06-24 00:02:31 +0700

    -

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { React, BrowserRouter as Router, Switch, Route } from 'libraries';
-import { appRoutes } from 'routes';
+import { React, BrowserRouter as Router, Switch, Route} from 'libraries';
+import { appRoutes, userRoutes } from 'routes';
+import './App.css';
 
 const App = () => {
   return (
     <Router>
+      {/* Routes */}
       <Switch>
-        {appRoutes.map(route => <Route {...route} />)}
+        {appRoutes.map((route, index) => <Route key={index} {...route} />)}
+        {userRoutes.map((route, index) => <Route key={index} {...route} />)}
       </Switch>
     </Router>
   );

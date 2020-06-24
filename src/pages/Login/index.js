10d88b6600a9bd43485cdf1c1022c8b7e1e59bf0
rifanid98 <adninsijawa@gmail.com>
2020-06-24 16:28:38 +0700

    grid fixed

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -16,12 +16,14 @@ export default class Login extends Component {
 			<Row>
 				<Col className={style.content} md="8">
 					<Row>
-						<Col className={style.caption}>
+						<Col md="2"></Col>
+						<Col className={style.caption} md="8">
 							<div className={style.title}>
 								<p>Book is a window</p>
 								<p>to the world</p>
 							</div>
 						</Col>
+						<Col md="2"></Col>
 					</Row>
 					<Row>
 						<Col md="2"></Col>
@@ -38,43 +40,45 @@ export default class Login extends Component {
 						<Col md="2"></Col>
 					</Row>
 					<Row className={style.contentFooter}>
-						<span>Photo by <a href="https://unsplash.com/@itfeelslikefilm?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Janko Ferlič</a> on <a href="/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+						<Col md="2"></Col>
+						<Col md="8">
+							<p>Photo by <a href="https://unsplash.com/@itfeelslikefilm?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Janko Ferlič</a> on <a href="/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></p>
+						</Col>
+						<Col md="2"></Col>
 					</Row>
 				</Col>
 				<Col className={style.sidebar} md="4">
-					<Row className={style.sidebarHeader}>
-						<p>Login
-						</p>
-							<small>Welcome back! Please login to your account.</small>
-					</Row>
-					<Row className={style.sidebarContent}>
-						<Form action="#">
-							<FormGroup>
-								<Label for="email">Email Address</Label>
-								<Input type="email" name="email" id="email" placeholder="example@domain.com" />
-							</FormGroup>
-							<FormGroup>
-								<Label for="password">Password</Label>
-								<Input type="password" name="password" id="password" placeholder="password" />
-							</FormGroup>
-							<FormGroup check>
-								<Label check>
-									<Input type="checkbox" />{' '}
-									<span style={{float: 'left', position: 'absolute', left: '17px'}}>Remember Me</span>
-									<span style={{ float: 'right', position: 'absolute', right: '0px' }}><Link to="/">Forgot Password</Link></span>
-								</Label>
-							</FormGroup>
-							<Button className={style.buttonLogin}>Login</Button>
-							<Button className={style.buttonSignup} outline onClick={(event) => {event.preventDefault(); this.props.history.push('/signup')}}>Signup</Button>
-						</Form>
-					</Row>
-					<Row className={style.sidebarFooter}>
-						<Col md={{ size: 8, offset: 2 }}>
-							<span>
-								
+					<Row>
+						<Col md="12">
+							<Row className={style.sidebarHeader}>
+								<p>Login</p>
+								<small>Welcome back! Please login to your account.</small>
+							</Row>
+							<Row className={style.sidebarContent}>
+								<Form action="#" className={style.form}>
+									<FormGroup>
+										<Label className={style.label} for="email">Email Address</Label>
+										<Input type="email" name="email" id="email" placeholder="example@domain.com" />
+									</FormGroup>
+									<FormGroup>
+										<Label className={style.label} for="password">Password</Label>
+										<Input type="password" name="password" id="password" placeholder="password" />
+									</FormGroup>
+									<FormGroup check>
+										<Label className={style.label} check>
+											<Input type="checkbox" />{' '}
+											<span className={style.span} style={{ float: 'left', position: 'absolute', left: '17px' }}>Remember Me</span>
+											<span className={style.span} style={{ float: 'right', position: 'absolute', right: '0px' }}><Link to="/">Forgot Password</Link></span>
+										</Label>
+									</FormGroup>
+									<Button className={style.buttonLogin}>Login</Button>
+									<Button className={style.buttonSignup} outline onClick={(event) => { event.preventDefault(); this.props.history.push('/signup') }}>Signup</Button>
+								</Form>
+							</Row>
+							<Row className={style.sidebarFooter}>
 								<p>By signing up, you agree to Book's <Link to="/">Terms and Conditions</Link> & <Link to="/">Privacy Policy</Link></p>
-							</span>
-					</Col>
+							</Row>
+						</Col>
 					</Row>
 				</Col>
 			</Row>

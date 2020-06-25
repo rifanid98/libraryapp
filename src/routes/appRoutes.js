0dad81891c7731f5a015b27fb6d71209bee4dc17
rifanid98 <adninsijawa@gmail.com>
2020-06-25 23:37:47 +0700

    detail book & homepage added

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import {Login } from '../pages'
-
-// import { Login, Signup } from "../pages";
+import {Login, Signup, Home, Detail } from '../pages'
 
 const notfound = () => {
   return (
@@ -12,17 +10,29 @@ const notfound = () => {
 const appRoutes = [
  
   {
+    name: "Detail",
+    component: Detail,
+    exact: true,
+    path: "/detail"
+  },
+  {
+    name: "Home",
+    component: Home,
+    exact: true,
+    path: "/home"
+  },
+  {
     name: "Login",
     component: Login,
     exact: true,
     path: "/login"
   },
-  // {
-  //   name: "Sign Up",
-  //   component: Signup,
-  //   exact: true,
-  //   path: "/signup"
-  // },
+  {
+    name: "Sign Up",
+    component: Signup,
+    exact: true,
+    path: "/signup"
+  },
   {
     name: "Not Found",
     component: notfound,

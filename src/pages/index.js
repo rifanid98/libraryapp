336f9a9f60ad67ed1958d1f1830d2791952b4c84
rifanid98 <adninsijawa@gmail.com>
2020-06-28 22:46:25 +0700

    new pages added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import Login from "./Login";
-// import Register from "./Register";
+import Signup from "./Signup";
+import Home from "./Home";
+import Detail from "./Detail";
+import Training from "./Training";
 
-// export { Login, Register };
-export { Login };
+export { 
+    Login, 
+    Signup, 
+    Home,
+    Detail,
+    Training
+ };

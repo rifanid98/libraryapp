d7e3faae247101264ecfe042ce16a748f270fef1
rifanid98 <adninsijawa@gmail.com>
2020-06-29 22:59:49 +0700

    slider component added

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,10 +2,12 @@ import AuthBannerTitle from './AuthBannerTitle';
 import AuthBannerCaption from './AuthBannerCaption';
 import AuthSidebarTitle from './AuthSidebarTitle';
 import AuthSidebarFooter from './AuthSidebarFooter';
+import SliderItem from './SliderItem';
 
 export {
     AuthBannerTitle,
     AuthBannerCaption,
     AuthSidebarTitle,
-    AuthSidebarFooter
+    AuthSidebarFooter,
+    SliderItem
 };

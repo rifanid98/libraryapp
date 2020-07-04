1101eeed3b60761bf627d687b285fc7bccfcdbc2
rifanid98 <adninsijawa@gmail.com>
2020-07-04 15:01:51 +0700

    small changes are not important

--- src/components/organisms/MyModal/index.js
+++ src/components/organisms/MyModal/index.js
@@ -19,14 +19,12 @@ const MyModal = (props) => {
 
     return (
         <div>
-            {/* <Button color="danger" onClick={toggle} id={id}>{buttonLabel}</Button> */}
             <span onClick={toggle} id={id}></span>
             <Modal isOpen={modal} toggle={toggle} className={className} size={size}>
                 <ModalHeader toggle={toggle}>{modalTitle}</ModalHeader>
                 <ModalBody>
-                    {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. */}
                     {modalBody}
-                    <Button color="secondary" className="float-right" onClick={toggle} style={{marginRight: '10px'}}>Cancel</Button>
+                    <Button color="secondary" className="float-right" onClick={toggle} style={{ marginRight: '10px' }}>Cancel</Button>
                 </ModalBody>
                 <ModalFooter>
                 </ModalFooter>

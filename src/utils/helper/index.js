d1c77002a171c871c3ec3320a6e7c3a8197fa480
rifanid98 <adninsijawa@gmail.com>
2020-07-04 15:13:28 +0700

    add decode jwt and convert iso date to human date helpers

--- src/utils/helper/index.js
+++ src/utils/helper/index.js
@@ -38,3 +38,21 @@ export const convertDate = date => {
   if (!date) return null;
   return moment(date).format("DD MMMM YYYY");
 };
+
+export const decodeJwtToken = jwtToken => {
+  const parts = jwtToken.split('.').map(part => Buffer.from(part.replace(/-/g, '+').replace(/_/g, '/'), 'base64').toString());
+  const payload = JSON.parse(parts[1]);
+  return payload;
+}
+
+export const convertISODate = (myDate, showTime = true) => {
+  const newDate = new Date(myDate);
+  const year = newDate.getFullYear();
+  const month = (newDate.getMonth() + 1) < 10 ? `0${newDate.getMonth() + 1}` : newDate.getMonth() + 1;
+  const date = newDate.getDate() < 10 ? `0${newDate.getDate()}` : newDate.getDate();
+  const hours = newDate.getHours() < 10 ? `0${newDate.getHours()}` : newDate.getHours();
+  const minutes = newDate.getMinutes() < 10 ? `0${newDate.getMinutes()}` : newDate.getMinutes();
+  const seconds = newDate.getSeconds() < 10 ? `0${newDate.getSeconds()}` : newDate.getSeconds();
+
+  return showTime ? `${year}-${month}-${date} ${hours}:${minutes}:${seconds}` : `${year}-${month}-${date}`;
+}

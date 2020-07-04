b28107701de18d9692f65a669e26995adb278645
rifanid98 <adninsijawa@gmail.com>
2020-07-04 15:02:33 +0700

    adding card footer to bookcard

--- src/components/organisms/BookCard/index.js
+++ src/components/organisms/BookCard/index.js
@@ -3,36 +3,37 @@ import React from 'react'
 
 // third party component
 import {
-    Col,
-    Card,
-    CardBody,
-    CardTitle,
-    CardText
+	Col,
+	Card,
+	CardBody,
+	CardTitle,
+	CardText
 } from 'reactstrap';
 
 // custom style
 import style from './style.module.css';
 
 const BookCard = (props) => {
-    const {
-        history,
-        bookId,
-        cardImage,
-        cardTitle,
-        cardText
-    } = props;
+	const {
+		history,
+		bookId,
+		cardImage,
+		cardTitle,
+		cardText,
+		cardFooter
+	} = props;
 
-    return (
-        <Col md="3" sm="4" xs="6" onClick={() => { history.push(`/detail/${bookId}`) }}>
-            <Card className={style.card} style={{ backgroundImage: `url(${cardImage})` }}>
-                <div className={style.imgWrapper}></div>
-                <CardBody className={style.cardBody}>
-                    <CardTitle className={style.cardTitle}>{cardTitle}</CardTitle>
-                    <CardText className={style.cardText}>{cardText}</CardText>
-                </CardBody>
-            </Card>
-        </Col>
-    )
+	return (
+		<Col md="4" sm="6" xs="12" onClick={() => { history.push(`/detail/${bookId}`) }}>
+			<Card className={style.card} style={{ backgroundImage: `url(${cardImage})` }}>
+				<div className={style.imgWrapper}></div>
+				<CardBody className={style.cardBody}>
+					<CardTitle className={style.cardTitle}>{cardTitle}</CardTitle>
+					<CardText className={style.cardText}>{cardText} <br /> {cardFooter}</CardText>
+				</CardBody>
+			</Card>
+		</Col>
+	)
 }
 
 export default BookCard;

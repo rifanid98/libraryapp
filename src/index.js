0035be6d324f0b1e79f51f32ab67d822fe00ac48
rifanid98 <adninsijawa@gmail.com>
2020-07-07 15:44:49 +0700

    apply redux persist

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from 'modules';
 
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import * as serviceWorker from './serviceWorker';
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>,
   </React.StrictMode>,
   document.getElementById('root')

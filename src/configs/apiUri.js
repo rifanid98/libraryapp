1e934d812181f6a934fae55347e6bcbc5b9a51f6
rifanid98 <adninsijawa@gmail.com>
2020-07-08 17:22:50 +0700

    custom api uri

--- src/configs/apiUri.js
+++ src/configs/apiUri.js
@@ -1,29 +1,48 @@
-const baseApiUri = 'http://localhost:3000/libraryapp-api';
+import appConfig from "./appConfig";
 
 const apiUri = {
   auth: {
-    login: `${baseApiUri}/auth/login`,
-    register: `${baseApiUri}/auth/register`
+    login: `${appConfig.url.api}/auth/login`,
+    register: `${appConfig.url.api}/auth/register`
   },
   books: {
-    getAllBooks: `${baseApiUri}/books`,
-    getDetailBook: `${baseApiUri}/books`,
-    postBook: `${baseApiUri}/books`,
-    patchBook: `${baseApiUri}/books`,
-    deleteBook: `${baseApiUri}/books`,
-    borrowBook: `${baseApiUri}/books`,
-    returnBook: `${baseApiUri}/books`
+    getAllBooks: `${appConfig.url.api}/books`,
+    getDetailBook: `${appConfig.url.api}/books`,
+    postBook: `${appConfig.url.api}/books`,
+    patchBook: `${appConfig.url.api}/books`,
+    deleteBook: `${appConfig.url.api}/books`,
+    borrowBook: `${appConfig.url.api}/books`,
+    returnBook: `${appConfig.url.api}/books`
   },
   genres: {
-    getAllGenres: `${baseApiUri}/genres`
+    getAllGenres: `${appConfig.url.api}/genres`
   },
+  users: `http://localhost:3000/libraryapp-api/users`,
+  // users: `${appConfig.url.api}/users`,
   histories: {
-    getAllHistories: `${baseApiUri}/histories`,
-    getPendingHistories: `${baseApiUri}/histories`
+    getAllHistories: `${appConfig.url.api}/histories`,
+    getPendingHistories: `${appConfig.url.api}/histories`
   },
   authors: {
-    getAllAuthors: `${baseApiUri}/authors`
+    getAllAuthors: `${appConfig.url.api}/authors`
   }
 }
 
 export { apiUri };
+
+
+
+// import { appConfig } from "configs";
+
+// const url = {
+//   books: `${appConfig.url.api}/books`,
+//   genres: `${appConfig.url.api}/genres`,
+//   authors: `${appConfig.url.api}/authors`,
+//   users: `${appConfig.url.api}/users`,
+//   auth: {
+//     login: `${appConfig.url.api}/auth/login`,
+//     register: `${appConfig.url.api}/auth/register`
+//   }
+// };
+
+// export default url;

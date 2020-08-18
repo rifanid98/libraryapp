9511908827ab0b157e1fa232689bd02980e786aa
rifanid98 <adninsijawa.office@gmail.com>
2020-08-18 13:52:39 +0700

    update api url

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://192.168.42.15:3000/libraryapp-api';
-export const ORIGIN = 'http://192.168.42.15:3000';
+export const API_URL = 'http://34.207.84.36/libraryapp-api';
+export const ORIGIN = 'http://34.207.84.36';
